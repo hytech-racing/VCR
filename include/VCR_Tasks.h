6aa1c79999f06855a338632eea3495168eaae106
Justin Hwang <justin@goliath.org>
2024-12-12 19:46:12 -0500

    Refactor: changed task naming conventions

--- include/VCR_Tasks.h
+++ include/VCR_Tasks.h
@@ -4,11 +4,10 @@
  * (https://wiki.hytechracing.org/books/ht09-design/page/ht-task-scheduler) for usage directions.
  * 
  * Generally, defining a task takes three steps:
- * 1) Define the "init" function. Name this function your task name with _init_function() after it.
- *    This init's function's inputs MUST be the same for all init functions, taking in sysMicros
- *    and a taskInfo reference.
- * 2) Define the "run" function. Name this function your task name with _run_function() after it.
- *    Similar to the init function, this function's inputs MUST be sysMicros and taskInfo.
+ * 1) Define the "init" function. Name this function init_<taskname>. This init funciton's inputs
+ *    MUST be the same for all init functions, taking in sysMicros and a taskInfo reference.
+ * 2) Define the "run" function. Name this function run_<taskname>. Similar to the init function,
+ *    this function's inputs MUST be sysMicros and taskInfo.
  * 3) Define the function itself. This requires using the HT_TASK::Task constructor and passing
  *    in your init function, your run function, a priority level, and a loop interval (in micros).
  * 
@@ -43,19 +42,19 @@
  * This "Test" function is purely for validation of the HT_SCHED dependency. This is intended to be removed when
  * further development occurs.
  */
-bool test_task_init_function(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
+bool init_test_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
     hal_printf("Initialized function at %d (micros)\n", sysMicros);
     return true;
 }
 
-bool test_task_run_function(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
+bool run_test_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
     hal_printf("Ran function at %d (micros) for the %dth time\n", sysMicros, taskInfo.executions);
     return true;
 }
 
-HT_TASK::Task test_task = HT_TASK::Task(test_task_init_function, test_task_run_function, 1, 100000UL); // 100,000us is 10hz
+HT_TASK::Task test_task = HT_TASK::Task(init_test_task, run_test_task, 1, 100000UL); // 100,000us is 10hz
 
 
 
@@ -65,7 +64,7 @@ HT_TASK::Task test_task = HT_TASK::Task(test_task_init_function, test_task_run_f
  * store them in structs defined in shared_firmware_types. This function relies on ADC_0 and
  * ADC_1 being defined in ADC_interface.h.
  */
-bool read_adc0_init_function(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
+bool init_read_adc0_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
 
     ADC_0.setChannelScaleAndOffset(GLV_SENSE_CHANNEL, GLV_SENSE_SCALE, GLV_SENSE_OFFSET);
@@ -81,7 +80,7 @@ bool read_adc0_init_function(const unsigned long& sysMicros, const HT_TASK::Task
     return true;
 }
 
-bool read_adc0_run_function(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
+bool run_read_adc0_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
 
     ADC_0.sample(); // Samples all eight channels.
@@ -91,6 +90,6 @@ bool read_adc0_run_function(const unsigned long& sysMicros, const HT_TASK::TaskI
     return true;
 }
 
-HT_TASK::Task read_adc0_task = HT_TASK::Task(test_task_init_function, test_task_run_function, 10, 1000UL); // 1000us is 1kHz
+HT_TASK::Task read_adc0_task = HT_TASK::Task(init_read_adc0_task, run_read_adc0_task, 10, 1000UL); // 1000us is 1kHz
 
 #endif /* VCR_TASKS */
