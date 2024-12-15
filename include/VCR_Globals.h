cb1b012a8c3335250c97604b1b390ce0dd2cd5bd
Justin Hwang <justin@goliath.org>
2024-12-14 20:22:05 -0500

    Switched buzzer reference to use singleton pattern instead of global instance

--- include/VCR_Globals.h
+++ include/VCR_Globals.h
@@ -7,9 +7,6 @@
 /* From shared-firmware-types */
 #include "SharedFirmwareTypes.h"
 
-/* Local includes */
-#include "Buzzer.h"
-
 /* Interface and system data structs */
 VCRInterfaceData_s interface_data;
 VCRSystemData_s system_data;
@@ -17,8 +14,5 @@ VCRSystemData_s system_data;
 /* ADC setup */
 MCP_ADC<8> adc_0 = MCP_ADC<8>(ADC0_CS); // MCP3208. ADC0 in VCR schematic. Used for valuable telem data.
 MCP_ADC<8> adc_1 = MCP_ADC<8>(ADC1_CS); // MCP3208. ADC1 in VCR schematic. Used for extra thermistors or extra sensors while testing.
-
-/* Buzzer setup */
-BuzzerController buzzer = BuzzerController::getInstance();
 
 #endif /* VCR_GLOBALS */
--- include/VCR_Tasks.h
+++ include/VCR_Tasks.h
@@ -38,6 +38,7 @@
 /* Local includes */
 #include "VCR_Constants.h"
 #include "VCR_Globals.h"
+#include "Buzzer.h"
 
 /**
  * This "Test" function is purely for validation of the HT_SCHED dependency. This is intended to be removed when
@@ -146,7 +147,7 @@ HT_TASK::Task read_adc1_task = HT_TASK::Task(init_read_adc1_task, run_read_adc1_
  */
 bool run_update_buzzer_controller_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
-    system_data.buzzer_is_active = buzzer.buzzer_is_active(sysMicros / 1000); // pass in sysMillis into buzzer_is_active check
+    system_data.buzzer_is_active = BuzzerController::getInstance().buzzer_is_active(sysMicros / 1000); // pass in sysMillis into buzzer_is_active check
 }
 
 HT_TASK::Task update_buzzer_controller_task = HT_TASK::Task(HT_TASK::DUMMY_FUNCTION, run_update_buzzer_controller_task, 5, 1000UL); // 1000us is 1kHz
