09021d0946e7f9eeeac52cef3b58b16daf482416
Justin Hwang <justin@goliath.org>
2024-12-18 19:59:12 -0800

    Added shell for safety system

---
+++ lib/systems/SafetySystem.cpp
@@ -0,0 +1,11 @@
+#include "SafetySystem.h"
+
+void SafetySystem::init()
+{
+    // TODO: Implement code
+}
+
+void SafetySystem::update_software_shutdown(unsigned long curr_millis)
+{
+    // TODO: Implement code
+}
