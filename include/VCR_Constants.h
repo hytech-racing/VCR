4a0f5787299a1f57e44625f130e64e95d9a3e7d0
Wangsiriwech, Javanith <jwangsiriwech3@gatech.edu>
2025-01-09 20:52:44 -0500

    Removed extraneous comments

--- include/VCR_Constants.h
+++ include/VCR_Constants.h
@@ -16,24 +16,24 @@ const int ADC1_CS = 11; // MCP3208. ADC1 in VCR schematic. Used for extra thermi
 /* -------------------------------------------------- */
 
 /* Channels on adc_0 */
-const int GLV_SENSE_CHANNEL       = 0; //1
-const int CURRENT_SENSE_CHANNEL   = 1; //2
-const int REFERENCE_SENSE_CHANNEL = 2; //3
-const int RL_LOADCELL_CHANNEL     = 3; //4
-const int RR_LOADCELL_CHANNEL     = 4; //5
-const int RL_SUS_POT_CHANNEL      = 5; //6
-const int RR_SUS_POT_CHANNEL      = 6; //7
-// const int UNUSED_CHANNEL       = 7; 8
+const int GLV_SENSE_CHANNEL       = 0;
+const int CURRENT_SENSE_CHANNEL   = 1;
+const int REFERENCE_SENSE_CHANNEL = 2;
+const int RL_LOADCELL_CHANNEL     = 3;
+const int RR_LOADCELL_CHANNEL     = 4;
+const int RL_SUS_POT_CHANNEL      = 5;
+const int RR_SUS_POT_CHANNEL      = 6;
+// const int UNUSED_CHANNEL       = 7;
 
 /* Channels on ADC_1 */
-const int THERMISTOR_0 = 0; //1
-const int THERMISTOR_1 = 1; //2
-const int THERMISTOR_2 = 2; //3
-const int THERMISTOR_3 = 3; //4
-const int THERMISTOR_4 = 4; //5
-const int THERMISTOR_5 = 5; //6
-const int THERMISTOR_6 = 6; //7
-const int THERMISTOR_7 = 7; //8
+const int THERMISTOR_0 = 0;
+const int THERMISTOR_1 = 1;
+const int THERMISTOR_2 = 2;
+const int THERMISTOR_3 = 3;
+const int THERMISTOR_4 = 4;
+const int THERMISTOR_5 = 5;
+const int THERMISTOR_6 = 6;
+const int THERMISTOR_7 = 7;
 
 /* Scaling and offset */
 const float GLV_SENSE_SCALE = (float)(24.0/((2.77149877/3.3)*4096.0)); //unsure about the multiplication by 4.0865
@@ -50,7 +50,6 @@ const float RR_LOADCELL_SCALE = 0.118f;
 const float RR_LOADCELL_OFFSET = 25.721f / RR_LOADCELL_SCALE;
 
 //does not matter that much
-//5/((3/3.3)*4096); //not sure if this also needs to be multiplied by 4.0865
 const float RL_SUS_POT_SCALE = 1;
 const int RL_SUS_POT_OFFSET = 1;
 const float RR_SUS_POT_SCALE = 1;
