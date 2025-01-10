ba3b18b06afe3f02e1a241e5d40326b10f95365b
Wangsiriwech, Javanith <jwangsiriwech3@gatech.edu>
2025-01-09 20:16:10 -0500

    Update VCR_Constants.h

--- include/VCR_Constants.h
+++ include/VCR_Constants.h
@@ -16,40 +16,45 @@ const int ADC1_CS = 11; // MCP3208. ADC1 in VCR schematic. Used for extra thermi
 /* -------------------------------------------------- */
 
 /* Channels on adc_0 */
-const int GLV_SENSE_CHANNEL       = 0;
-const int CURRENT_SENSE_CHANNEL   = 1;
-const int REFERENCE_SENSE_CHANNEL = 2;
-const int RL_LOADCELL_CHANNEL     = 3;
-const int RR_LOADCELL_CHANNEL     = 4;
-const int RL_SUS_POT_CHANNEL      = 5;
-const int RR_SUS_POT_CHANNEL      = 6;
-// const int UNUSED_CHANNEL       = 7;
+const int GLV_SENSE_CHANNEL       = 0; //1
+const int CURRENT_SENSE_CHANNEL   = 1; //2
+const int REFERENCE_SENSE_CHANNEL = 2; //3
+const int RL_LOADCELL_CHANNEL     = 3; //4
+const int RR_LOADCELL_CHANNEL     = 4; //5
+const int RL_SUS_POT_CHANNEL      = 5; //6
+const int RR_SUS_POT_CHANNEL      = 6; //7
+// const int UNUSED_CHANNEL       = 7; 8
 
 /* Channels on ADC_1 */
-const int THERMISTOR_0 = 0;
-const int THERMISTOR_1 = 1;
-const int THERMISTOR_2 = 2;
-const int THERMISTOR_3 = 3;
-const int THERMISTOR_4 = 4;
-const int THERMISTOR_5 = 5;
-const int THERMISTOR_6 = 6;
-const int THERMISTOR_7 = 7;
+const int THERMISTOR_0 = 0; //1
+const int THERMISTOR_1 = 1; //2
+const int THERMISTOR_2 = 2; //3
+const int THERMISTOR_3 = 3; //4
+const int THERMISTOR_4 = 4; //5
+const int THERMISTOR_5 = 5; //6
+const int THERMISTOR_6 = 6; //7
+const int THERMISTOR_7 = 7; //8
 
 /* Scaling and offset */
-const float GLV_SENSE_SCALE = -1; //TODO: FIGURE THIS OUT
-const int GLV_SENSE_OFFSET = -1; //TODO: FIGURE THIS OUT
-const float CURRENT_SENSE_SCALE = -1; //TODO: FIGURE THIS OUT
-const int CURRENT_SENSE_OFFSET = -1; //TODO: FIGURE THIS OUT
-const float REFERENCE_SENSE_SCALE = -1; //TODO: FIGURE THIS OUT
-const int REFERENCE_SENSE_OFFSET = -1; //TODO: FIGURE THIS OUT
-const float RL_LOADCELL_SCALE = -1; //TODO: FIGURE THIS OUT
-const int RL_LOADCELL_OFFSET = -1; //TODO: FIGURE THIS OUT
-const float RR_LOADCELL_SCALE = -1; //TODO: FIGURE THIS OUT
-const int RR_LOADCELL_OFFSET = -1; //TODO: FIGURE THIS OUT
-const float RL_SUS_POT_SCALE = -1; //TODO: FIGURE THIS OUT
-const int RL_SUS_POT_OFFSET = -1; //TODO: FIGURE THIS OUT
-const float RR_SUS_POT_SCALE = -1; //TODO: FIGURE THIS OUT
-const int RR_SUS_POT_OFFSET = -1; //TODO: FIGURE THIS OUT
+const float GLV_SENSE_SCALE = 24/((2.77149877/3.3)*4096); //unsure about the multiplication by 4.0865
+// const int GLV_SENSE_OFFSET = -1; //No offset for GLV
+const float CURRENT_SENSE_SCALE = 24/((2.77149877/3.3)*4096); //unsure about the multiplication by 4.0865
+// const int CURRENT_SENSE_OFFSET = -1; //No offset for CURRENT_SENSE
+const float REFERENCE_SENSE_SCALE = 24/((2.77149877/3.3)*4096); //unsure about the multiplication by 4.0865
+// const int REFERENCE_SENSE_OFFSET = -1; //No offset for REFERENCE_SENSE
+
+//Values are from the old MCU rev15
+const float LOADCELL_RL_SCALE = 0.1149;
+const float LOADCELL_RL_OFFSET = 13.526 / LOADCELL_RL_SCALE;
+const float LOADCELL_RR_SCALE = 0.118;
+const float LOADCELL_RR_OFFSET = 25.721 / LOADCELL_RR_SCALE;
+
+//does not matter that much
+//5/((3/3.3)*4096); //not sure if this also needs to be multiplied by 4.0865
+const float RL_SUS_POT_SCALE = 1
+const int RL_SUS_POT_OFFSET = 1; //TODO: FIGURE THIS OUT
+const float RR_SUS_POT_SCALE = 1; //TODO: FIGURE THIS OUT
+const int RR_SUS_POT_OFFSET = 1; //TODO: FIGURE THIS OUT
 
 
 
