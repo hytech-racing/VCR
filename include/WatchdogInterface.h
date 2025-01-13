d2a7b7d6a98307e87a6b46de5b2a252ab8767a9f
anshulnaini <anshuln098@gmail.com>
2025-01-12 22:21:08 -0500

    Minor updates
    - deleted assignment operator as requested
    
    - followed c-style brackets
    
    -private member vars are now preceded with "_"

--- include/WatchdogInterface.h
+++ include/WatchdogInterface.h
@@ -1,6 +1,20 @@
 #ifndef __WATCHDOG_INTERFACE_H__
 #define __WATCHDOG_INTERFACE_H__
 
+
+/**
+ * This class controls the watchdog timer This class is implemented as a singleton to ensure only one instance of the watchdog timer is active in the system.
+ * The WatchdogInterface provides functionality to initialize, monitor, and "kick" the watchdog to prevent system resets.
+ * 
+ * NOTE:  The WatchdogInterface ensures system responsiveness by requiring periodic "kicks" using the `kick_watchdog()` method. 
+ * During initialization, the watchdog pin is configured, and its state is set. If the timer is not reset within the 
+ * `WATCHDOG_KICK_INTERVAL`, it assumes the system is unresponsive and may trigger a reset. The singleton design ensures 
+ * only one instance manages the watchdog, which will help mantain consistency and control.
+ * 
+ * IMPORTANT - must call setPin() before init()
+ */
+
+
 #include <Arduino.h>
 
 const unsigned long WATCHDOG_KICK_INTERVAL = 10;    // milliseconds
@@ -10,21 +24,22 @@ class WatchdogInterface
 private:
 
     /*Private constructor*/
-    WatchdogInterface(): pin_watchdog_input_(-1){};
+    WatchdogInterface(): _pin_watchdog_input_(-1){};
 
     /* Watchdog last kicked time */
-    unsigned long watchdog_time;
+    unsigned long _watchdog_time;
 
     /* Watchdog output state */
-    bool watchdog_state;
+    bool _watchdog_state;
 
     /* Hardware interface pins */
-    int pin_watchdog_input_;
+    int _pin_watchdog_input_;
     
 public:
 
     /*Getter to return the instance and make a new one if it hasn't been made yet*/
-    static WatchdogInterface& getInstance() {
+    static WatchdogInterface& getInstance() 
+    {
         static WatchdogInterface instance;
         return instance;
     }
@@ -35,6 +50,9 @@ public:
 
     /*Prevent copying*/
     WatchdogInterface(const WatchdogInterface&) = delete;
+
+    /*Prevent Assignment*/
+    WatchdogInterface& operator= (const WatchdogInterface&) = delete;
 
 
     /* Initialize interface pin mode */
--- src/WatchdogInterface.cpp
+++ src/WatchdogInterface.cpp
@@ -3,43 +3,49 @@
 
 /* Pin mode output to watchdog WD */
 /*Pin MUST be set with setPin() before calling init()*/
-void WatchdogInterface::init(unsigned long curr_millis) {
+void WatchdogInterface::init(unsigned long curr_millis) 
+{
     // Set pin mode        
-    pinMode(pin_watchdog_input_, OUTPUT);
+    pinMode(_pin_watchdog_input_, OUTPUT);
 
-    watchdog_time = curr_millis;
+    _watchdog_time = curr_millis;
     set_watchdog_state(HIGH);
 }
 
 /* Initial output to watchdog WD */
-void WatchdogInterface::set_start_state() {
+void WatchdogInterface::set_start_state() 
+{
 
-    digitalWrite(pin_watchdog_input_, HIGH);
+    digitalWrite(_pin_watchdog_input_, HIGH);
     
 }
 
 /* Toggle watchdog WD to kick dog */
-void WatchdogInterface::kick_watchdog(unsigned long curr_millis) {
+void WatchdogInterface::kick_watchdog(unsigned long curr_millis) 
+{
 
-    if ((curr_millis - watchdog_time) > WATCHDOG_KICK_INTERVAL) {
-        watchdog_state = !watchdog_state;
-        digitalWrite(pin_watchdog_input_, watchdog_state);
-        watchdog_time = curr_millis;
+    if ((curr_millis - _watchdog_time) > WATCHDOG_KICK_INTERVAL) {
+        _watchdog_state = !_watchdog_state;
+        digitalWrite(_pin_watchdog_input_, _watchdog_state);
+        _watchdog_time = curr_millis;
     }
 
 }
 
 /* Get interface status */
-bool WatchdogInterface::get_watchdog_state() {
-    return watchdog_state;
+bool WatchdogInterface::get_watchdog_state() 
+{
+    return _watchdog_state;
 }
 
 /* Set interface status */
-void WatchdogInterface::set_watchdog_state(bool state) {
-    watchdog_state = state;
+void WatchdogInterface::set_watchdog_state(bool state) 
+{
+    _watchdog_state = state;
 }
 
 /*Set the pin from default (-1) of the Watchdog after calling getInstance() the first time*/
-void WatchdogInterface::setPin(int pin){
-    pin_watchdog_input_ = pin;
+void WatchdogInterface::setPin(int pin)
+{
+    _pin_watchdog_input_ = pin;
 }
