149fe203153a7965156ae3ca78ede0dbf8857326
Vansh Aggarwal <vanshagg233@gmail.com>
2025-02-02 17:44:12 -0500

    Moved Files to correct location

--- lib/systems/include/AMSSystem.h
+++ lib/systems/include/AMSSystem.h
@@ -1,116 +1,122 @@
-//Previously known as AMS Interface in MCU (HT08)
-
 #ifndef __AMSSYSTEM_H__
 #define __AMSSYSTEM_H__
 
-#include <Arduino.h>
-#include <cstdint>
+#include <stdint.h>
+#include <SharedFirmwareTypes.h>
 
-/* Heartbeat Interval is the allowable amount of time between BMS status messages before car delatches */
-const unsigned long HEARTBEAT_INTERVAL                      = 2000;   // milliseconds
-/* The total pcc threshold is the lowest allowable voltage of the entire pack (in Volts)*/
-const unsigned long PACK_CHARGE_CRIT_TOTAL_THRESHOLD        = 420;
+/* Heartbeat Interval is the allowable amount of milliseconds between BMS status messages before car delatches */
+const unsigned long HEARTBEAT_INTERVAL_MS                  = 2000;    // milliseconds
+
+/* The total PCC threshold is the lowest allowable voltage of the entire pack (in Volts)*/
+const unsigned long PACK_CHARGE_CRIT_TOTAL_THRESHOLD_VOLTS = 420;
+
 /* The lowest pcc threshold is the lowest allowable single cell voltage (in 100 microvolts)*/
-const unsigned long PACK_CHARGE_CRIT_LOWEST_CELL_THRESHOLD  = 35000; //equivalent to 3.5V
+const unsigned long PACK_CHARGE_CRIT_LOWEST_CELL_THRESHOLD = 35000;   // Equivalent to 3.5V
 
-const float DEFAULT_INIT_TEMP       = 40.0;
-const float DEFAULT_INIT_VOLTAGE    = 3.5;
-const float DEFAULT_TEMP_ALPHA      = 0.8;
-const float DEFAULT_VOLTAGE_ALPHA   = 0.8;
-const uint16_t MAX_PACK_CHARGE      = 48600;
-const unsigned long DEFAULT_INITIALIZATION_WAIT_INTERVAL = 5000;
-const int DEFAULT_SW_OK_PIN_ = 37; //number from scehmatic for VCR
+const float DEFAULT_INIT_TEMP       = 40.0;                           // Celsius
+const float DEFAULT_INIT_VOLTAGE    = 3.5;                            // Volts
+const float DEFAULT_TEMP_ALPHA      = 0.8;                            // IIR filter alpha
+const float DEFAULT_VOLTAGE_ALPHA   = 0.8;                            // IIR filter alpha
+const uint16_t MAX_PACK_CHARGE      = 48600;                          // Coulombs
 
 
-/// @brief this class is for interfacing with the AMS (accumulator management system) 
+/**
+ * Singleton class for communicating with the BMS. If one of the shutdown conditions is met, this class will return a
+ * false for get_bms_ok.
+ * 
+ * Shutdown conditions:
+ * 1) Has not received a message from BMS for more than "HEARTBEAT_INTERVAL_MS" milliseconds.
+ * 2) Total pack voltage is below critical threshold.
+ * 3) Lowest cell voltage is below critical threshold.
+ */
 class AMSSystem
 {
 public:
-    /* Method to get instance of object */
+
+    /**
+     * Retrieves the singular instance of the AMSSystem
+     */
     static AMSSystem& getInstance()
     {
         static AMSSystem instance;
         return instance;
     }
 
-    // ***Only use if needed***
-    // // Delete copy constructor and assignment operator to prevent duplication. // Added
-    // AMSInterface(const AMSInterface&) = delete; // Added
-    // AMSInterface& operator=(const AMSInterface&) = delete; // Added
+    /**
+     * Delete copy constructor and assignment operator to prevent duplication.
+     */
+    AMSSystem(const AMSSystem&) = delete;
+    AMSSystem& operator=(const AMSSystem&) = delete;
 
-    /* Initialize the heartbeat timer */
-    void init(unsigned long curr_micros);//unsigned long micros
+    /**
+     * Initialize the heartbeat timer.
+     */
+    void init(unsigned long curr_micros);
 
-    /* Init software OK pin by setting high*/
-    void set_start_state();
-
-    /* Check if the last heartbeat arrived within allowable interval */
-    bool heartbeat_received(unsigned long curr_micros);//micros
-
-    /* Check if either lowest cell or total pack is below threshold*/
-    bool pack_charge_is_critical(); 
-
-    //SETTERS//    
-    /* set software OK pin */
-    void set_state_ok_high(bool ok_high);    
-    /* set the last heartbeat to the current millis time */
-    void set_heartbeat(unsigned long curr_millis);
-
-    //GETTERS//
-    /* IIR filter and return filtered max cell temperature */
-    float get_filtered_max_cell_temp();
-    /* IIR filter and return filtered min cell voltage */
-    float get_filtered_min_cell_voltage();
-    /*gets the derate factor for acc system*/
-    float get_acc_derate_factor();
-
-    /*Updates Acc_derate_factor*/
-    void calculate_acc_derate_factor();
-    
+    AMSSystemData_s update_ams_system(unsigned long curr_millis, BMSData_s &bms_data);
 
 private:
 
-    // Private constructor for the singleton pattern
-    /*!
-        Constructor for the AMS Interface
-        @param sw_ok_pin The software ok pin number.
-        This pin is connected to the shutdown line and will go low if the AMS times out
-    */
-    AMSSystem(int sw_ok_pin, float init_temp, float init_volt, float temp_alpha, float volt_alpha):        
-        _pin_software_ok(sw_ok_pin),
-        filtered_max_cell_temp(init_temp),
-        filtered_min_cell_voltage(init_volt),
-        cell_temp_alpha(temp_alpha),
-        cell_voltage_alpha(volt_alpha) {};
-
-    /* Overloaded constructor that only takes in software OK pin and uses default voltages and temp*/
-    AMSSystem():
-        AMSSystem(DEFAULT_SW_OK_PIN_, DEFAULT_INIT_TEMP, DEFAULT_INIT_VOLTAGE, DEFAULT_TEMP_ALPHA, DEFAULT_VOLTAGE_ALPHA) {};
-
-    /* software OK pin */
-    int _pin_software_ok;
+    /**
+     * Constructor for the AMS Interface
+     */
+    AMSSystem() :
+        _last_heartbeat_time(0),
+        _filtered_max_cell_temp(DEFAULT_INIT_TEMP),
+        _filtered_min_cell_voltage(DEFAULT_INIT_VOLTAGE),
+        _cell_temp_alpha(DEFAULT_TEMP_ALPHA),
+        _cell_voltage_alpha(DEFAULT_VOLTAGE_ALPHA)
+    {};
 
     /* AMS last heartbeat time */
-    unsigned long last_heartbeat_time_;
+    unsigned long _last_heartbeat_time;
     
-    /* IIR filter parameters */
-    float bms_high_temp;
-    float bms_low_voltage;
-    float filtered_max_cell_temp;
-    float filtered_min_cell_voltage;
-    float cell_temp_alpha;
-    float cell_voltage_alpha;
+    float _filtered_max_cell_temp;
+    float _filtered_min_cell_voltage;
 
-    float acc_derate_factor;
+    /* extra filtered information */
+    float _filtered_average_cell_voltage;
+    float _filtered_max_cell_voltage;
+    float _filtered_average_cell_temp;
+    float _filtered_min_cell_temp;
 
-    // Check if lowest cell temperature is below threshold
+    /* IIR alpha values */
+    float _cell_temp_alpha;
+    float _cell_voltage_alpha;
+
+    /*Storage of BMS Struct*/
+    BMSData_s bms_container;
+
+
+    /* Check if lowest cell temperature is below threshold */
     bool is_below_pack_charge_critical_low_thresh();
-    // Check if total pack charge is above threshold
+
+    /* Check if total pack charge is above threshold */
     bool is_below_pack_charge_critical_total_thresh();
+
+    /* IIR filter and return filtered max cell temperature */
+    float get_filtered_max_cell_temp();
+    /* IIR filter and return filtered min cell voltage */
+    float get_filtered_min_cell_voltage();    
+
+
+    /* IIR filter and return filtered average cell voltage */
+    float get_filtered_average_cell_voltage();
+    /* IIR filter and return filtered max cell voltage */
+    float get_filtered_max_cell_voltage();
+    /* IIR filter and return filtered average cell temp */
+    float get_filtered_average_cell_temp();
+    /* IIR filter and return filtered min cell temp */
+    float get_filtered_min_cell_temp();
+
+
+    //Checkers (return true if everything is good)
+    /* Checks if "Total pack voltage is below critical threshold" */
+    bool check_voltage();
+
+    /* Check if heartbeat received is within interval allowance */
+    bool check_heartbeat(unsigned long curr_micros);
 
 };
 
 #endif /* __AMSSYSTEM_H__ */
-
-//chage all private variables to be preceded
-//look at linter from main branch
