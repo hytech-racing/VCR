12fc08d8b34245d7e5617400f7f35f2b7aaa73a7
Ben Hall <rcmast3r1@gmail.com>
2025-02-02 19:04:46 -0500

    Fix/watchdog fix (#9)
    * (fix) making test better and making clear singleton for watchdog impl
    
    * (fix) ignoring linter issue and fixing typo

--- include/VCR_Globals.h
+++ include/VCR_Globals.h
@@ -19,7 +19,5 @@ constexpr unsigned int channels_within_mcp_adc = 8;
 extern MCP_ADC<channels_within_mcp_adc> adc_0; // MCP3208. ADC0 in VCR schematic. Used for valuable telem data.
 extern MCP_ADC<channels_within_mcp_adc> adc_1; // MCP3208. ADC1 in VCR schematic. Used for extra thermistors or extra sensors while testing.
 
-/* Watchdog pin */
-extern const int watchdog_pin;
 
 #endif /* VCR_GLOBALS */
--- lib/systems/include/WatchdogSystem.h
+++ lib/systems/include/WatchdogSystem.h
@@ -1,51 +1,39 @@
 #ifndef WATCHDOG_SYSTEM_H
 #define WATCHDOG_SYSTEM_H
 
+#include <etl/singleton.h>
 
 /**
  * This class controls the boolean _watchdog_state, but does not directly control the watchdog.
  * WatchdogSystem provides functionality to initialize, monitor, and "kick" the watchdog to prevent system resets.
  * 
  * NOTE:  To ensure system responsiveness, WatchdogSystem requires periodic updates by calling the `get_watchdog_state()` method. 
- * This toggles the _watchdog_state (if the interval has passed) and returns the new state, which must then be sent to the watchdog. 
- * If this is not done within the `WATCHDOG_KICK_INTERVAL`, it assumes the system is unresponsive and may trigger a reset. The singleton design ensures 
- * only one instance manages the watchdog, which will help mantain consistency and control.
- * 
+ * This toggles the _watchdog_state (if the interval has passed) and returns the new state, which must then be sent to the watchdog.  
  */
 
-const unsigned long WATCHDOG_KICK_INTERVAL = 10;    // milliseconds
 
 class WatchdogSystem
 {
+public:
+    
+    WatchdogSystem(const unsigned long kick_interval_ms = 10UL) : _watchdog_time(0), _watchdog_state(false), _watchdog_kick_interval(kick_interval_ms) {};
 private:
 
     /*Private constructor*/
-    WatchdogSystem() : _watchdog_time(0), _watchdog_state(false) {};
 
     /* Watchdog last kicked time */
     unsigned long _watchdog_time;
-
-    /* Watchdog output state */
     bool _watchdog_state;
+    unsigned long _watchdog_kick_interval;
+    /* Watchdog output state */
     
 public:
-
-    /*Getter to return the instance and make a new one if it hasn't been made yet*/
-    static WatchdogSystem& getInstance() 
-    {
-        static WatchdogSystem instance;
-        return instance;
-    }
-
-    /*Prevent copying*/
-    WatchdogSystem(const WatchdogSystem&) = delete;
-
-    /*Prevent Assignment*/
-    WatchdogSystem& operator= (const WatchdogSystem&) = delete;
 
     /* Get/update watchdog state */
     bool get_watchdog_state(unsigned long curr_millis);
 
 };
+
+using WatchdogInstance = etl::singleton<WatchdogSystem>;
 
 #endif /* WATCHDOG_SYSTEM_H */
