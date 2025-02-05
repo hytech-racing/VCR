809d05b9134033a958c723d1abe426d34b23d956
Justin Hwang <justin@goliath.org>
2025-02-04 19:23:02 -0500

    Added test for scheduler wraparound

--- test/test_systems/test_scheduler.h
+++ test/test_systems/test_scheduler.h
@@ -17,13 +17,13 @@ unsigned long stdMicros()
 
 bool init_test_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
-    hal_printf("Initialized function at %d (micros)\n", sysMicros);
+    // hal_printf("Initialized function at %d (micros)\n", sysMicros);
     return true;
 }
 
 bool run_test_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
-    hal_printf("Ran function at %d (micros) for the %dth time\n", sysMicros, taskInfo.executions);
+    // hal_printf("Ran function at %d (micros) for the %dth time\n", sysMicros, taskInfo.executions);
     return true;
 }
 HT_TASK::Task test_task = HT_TASK::Task(init_test_task, run_test_task, 1, 100000UL); // 100,000us is 10hz
@@ -43,6 +43,40 @@ TEST(SchedulerTest, basic_functionality)
         scheduler.run();
     }
     
+}
+
+
+unsigned int timestamp = (unsigned int) (4294967296UL - 10000UL); // Start 10ms early
+unsigned long testMicrosFunction() // Increments by 100 microseconds every function call
+{
+    return timestamp;
+}
+
+int num_runs = 0;
+bool run_test_overflow_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
+{
+    num_runs++;
+    hal_printf("Ran task for %uth time\n", num_runs);
+    return true;
+}
+HT_TASK::Task test_overflow_task = HT_TASK::Task(HT_TASK::DUMMY_FUNCTION, run_test_overflow_task, 1, 1000UL); // 1,000us is 1kHz
+
+TEST(SchedulerTest, microsFunctionOverflow)
+{
+    HT_SCHED::Scheduler& scheduler = HT_SCHED::Scheduler::getInstance();
+    scheduler.setTimingFunction(testMicrosFunction);
+
+    scheduler.schedule(test_overflow_task);
+
+    for (int i = 1; i < 50; ++i) { // Run 30 cycles of the overflow task
+        for (int j = 0; j < 10; ++j) {
+            scheduler.run();
+            timestamp += 100; // Increment timestamp by 100ms every time
+        }
+        hal_printf("Timestamp is %u\n", timestamp);
+        ASSERT_EQ(num_runs, i);
+    }
+
 }
 
 #endif /* SCHEDULER_TEST */
