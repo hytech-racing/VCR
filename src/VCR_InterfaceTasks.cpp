0f8427e7e01a26f84c0015e0a877ebb7d2174754
Ben Hall <rcmast3r1@gmail.com>
2025-02-11 20:16:10 -0500

    re-adjusting place of buzzer status

--- src/VCR_InterfaceTasks.cpp
+++ src/VCR_InterfaceTasks.cpp
@@ -91,7 +91,7 @@ HT_TASK::Task read_adc1_task = HT_TASK::Task(init_read_adc1_task, run_read_adc1_
 bool run_update_buzzer_controller_task(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo)
 {
 
-    vcr_data.interface_data.buzzer_is_active = BuzzerController::getInstance().buzzer_is_active(sysMicros / 1000); //NOLINT
+    vcr_data.system_data.buzzer_is_active = BuzzerController::getInstance().buzzer_is_active(sysMicros / 1000); //NOLINT
 
     return true;
 }
