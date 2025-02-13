296628d78c334bd44f466c2ba7d72a4c2a3e2ccd
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2025-02-12 21:45:48 -0500

    builds

--- src/main.cpp
+++ src/main.cpp
@@ -26,32 +26,6 @@
 /* Scheduler setup */
 HT_SCHED::Scheduler& scheduler = HT_SCHED::Scheduler::getInstance();
 
-
-// /* Scheduler setup */
-// HT_SCHED::Scheduler& scheduler = HT_SCHED::Scheduler::getInstance();
-
-// /* Ethernet message sockets */ // TODO: Move this into its own interface
-// qindesign::network::EthernetUDP protobuf_send_socket;
-// qindesign::network::EthernetUDP protobuf_recv_socket;
-
-// // InverterInterface mci1_interface = InverterInterface(&CAN1_rxBuffer, 1, 2);
-
-// void setup() {
-//     scheduler.setTimingFunction(micros);
-
-//     scheduler.schedule(tick_state_machine_task);
-//     scheduler.schedule(read_adc0_task);
-//     scheduler.schedule(read_adc1_task);
-//     scheduler.schedule(update_buzzer_controller_task);
-// }
-
-    scheduler.schedule(tick_state_machine_task);
-    scheduler.schedule(read_adc0_task);
-    scheduler.schedule(read_adc1_task);
-    scheduler.schedule(update_buzzer_controller_task);
-    scheduler.schedule(kick_watchdog_task);
-}
-
 void setup(void)
 {
     while (!Serial)
@@ -64,62 +38,5 @@ bool ran_test = false;
 void loop()
 {
 
-    // TEST SEND
-
-    Serial.println("=== TESTING SEND ===");
-
-    Serial.println("Adding torque command to tx buffer...");
-
-    fl_inv.send_MC_TORQUE_COMMAND();
-
-    Serial.print("Tx buffer size (should be 1) ");
-    Serial.println(CAN3_txBuffer.size());
-
-    Serial.println("Sending CAN messages via can interface...");
-    send_all_CAN_msgs(CAN3_txBuffer, &TEST_CAN3);
-
-    Serial.print("Tx buffer size (should be 0): ");
-    Serial.println(CAN3_txBuffer.size());
-
-    delay(100);
-
-    Serial.println("");
-
-    // Serial.println("=== TESTING RECIEVE ===");
-
-    // Serial.print("Rx buffer size before processing: ");
-    // Serial.println(CAN3_rxBuffer.size());
-
-    // process_ring_buffer<CircularBufferType>(CAN3_rxBuffer, CAN_interfaces);
-
-    // Serial.print("Rx buffer size after processing (shoud be 0): ");
-    // Serial.println(CAN3_rxBuffer.size());
-
-    // Serial.println("");
-    // Serial.println("");
-
-
-
-    // for (uint8_t i = 0; i < 8; i++)
-    //     msg.buf[i] = i + 1;
-    // TEST_CAN3.write(msg);
-    // ran_test = true;
-    // delay(10);
-    // Serial.println("======= TEST 1 =======");
-    // Serial.println("can3 rx buffer size: (should be 1)");
-    // Serial.println(CAN3_rxBuffer.available());
-    // Serial.println("======= TEST 2 =======");
-    // Serial.println("can1 received message equal to sent msg");
-    // delay(10);
-    // if (CAN1_rxBuffer.available())
-    // {
-    //     CAN_message_t recvd_msg;
-    //     uint8_t buf[sizeof(CAN_message_t)];
-    //     CAN1_rxBuffer.pop_front(buf, sizeof(CAN_message_t));
-    //     memmove(&recvd_msg, buf, sizeof(recvd_msg));
-
-    //     Serial.println("\tid = prev: (should be 1)");
-    //     Serial.print("\t");
-    //     Serial.println(recvd_msg.id == msg.id);
-    // }
+  
 }
