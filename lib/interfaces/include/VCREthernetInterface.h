7208416942fd85e35986ea51428a11b6f652f5a5
Justin Hwang <justin@goliath.org>
2025-02-13 23:52:13 -0500

    added veh vec copy helper function

--- lib/interfaces/include/VCREthernetInterface.h
+++ lib/interfaces/include/VCREthernetInterface.h
@@ -22,7 +22,7 @@ namespace VCREthernetInterface
      * 
      * @post After this function completes, shared_state will have updated contents of ACUCoreData.
      */
-    void receive_pb_msg_acu_core_data(const hytech_msgs_ACUCoreData_s &msg_in, VCRData_s &shared_state);
+    void receive_pb_msg_acu_core_data(const hytech_msgs_ACUCoreData_s &msg_in, VCRData_s &shared_state, unsigned long curr_millis);
 
     /**
      * Function to take a populated protoc struct from ACU and update the VCR state.
@@ -42,7 +42,7 @@ namespace VCREthernetInterface
      * 
      * @post After this function completes, shared_state will have updated contents of ACUAllData.
      */
-    void receive_pb_msg_db(const hytech_msgs_MCUCommandData &msg_in, VCRData_s &shared_state);
+    void receive_pb_msg_db(const hytech_msgs_MCUCommandData &msg_in, VCRData_s &shared_state, unsigned long curr_millis);
 
     /**
      * Function to take a populated protoc struct from VCF and update the VCR state.
@@ -52,15 +52,31 @@ namespace VCREthernetInterface
      * 
      * @post After this function completes, shared_state will have updated contents of ACUAllData.
      */
-    void receive_pb_msg_vcf(const hytech_msgs_VCFData_s &msg_in, VCRData_s &shared_state);
+    void receive_pb_msg_vcf(const hytech_msgs_VCFData_s &msg_in, VCRData_s &shared_state, unsigned long curr_millis);
 
     /**
      * Helper function to copy an instance of InverterData_s to the protoc struct hytech_msgs_InverterData_s.
      * @param original A populated instance of the InverterData_s defined in shared firmware types.
      * @param destination The destination protoc struct.
-     * @post The destination struct will be populated with the data from original.s
+     * @post The destination struct will be populated with the data from original.
      */
     void copy_inverter_data(InverterData_s &original, hytech_msgs_InverterData_s &destination);
+
+    /**
+     * Helper function to copy veh_vec data.
+     * 
+     * @param original A populated instance of a veh_vec.
+     * @param destination A reference to an unpopulated instance of veh_vec.
+     * @post The destination veh_vec will be populated with the data from the original.
+     */
+    template <typename from_T, typename to_T>
+    void copy_veh_vec_members(const from_T& from, to_T& to)
+    {
+      to.FL = from.FL;
+      to.FR = from.FR;
+      to.RL = from.RL;
+      to.RR = from.RR;
+    }
 }
 
 #endif /* VCR_ETHERNET_INTERFACE_H */
