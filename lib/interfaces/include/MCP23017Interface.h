e42f1e6e69dd0021fd950b900fbb2e410d7fa236
anonimous-user <harrisbond1234@gmail.com>
2025-02-16 22:11:49 -0500

    fixed problem with input type

--- lib/interfaces/include/MCP23017Interface.h
+++ lib/interfaces/include/MCP23017Interface.h
@@ -1,12 +1,12 @@
 #ifndef __MCP23017INTERFACE_H__
 #define __MCP23017INTERFACE_H__
 
-#include <sys/types.h>
 
-// 
+#include "SharedFirmwareTypes.h"
+
 namespace specifiedBit
 {
-    bool getBit(u_int16_t data, bool port, int bit);
+    bool getBit(uint16_t data, bool port, int bit);
 }
 
 #endif // __MCP23017INTERFACE_H__
