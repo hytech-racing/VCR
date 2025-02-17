ac762c2774111f89cb964c4c865c221e0cde32d6
Bhardwaj, Mansi <mbhardwaj41@gatech.edu>
2025-02-16 23:40:46 -0500

    updated ADC implementation to use singleton class

--- include/VCR_Globals.h
+++ include/VCR_Globals.h
@@ -1,6 +1,8 @@
 #ifndef VCR_GLOBALS
 #define VCR_GLOBALS
 
+#include "etl/singleton.h"
+
 /* From shared-firmware-interfaces */
 #include "MCP_ADC.h"
 
@@ -15,8 +17,8 @@ extern VCRData_s vcr_data; // NOLINT
 
 /* ADC setup */
 constexpr unsigned int channels_within_mcp_adc = 8;
-extern MCP_ADC<channels_within_mcp_adc> adc_0; // MCP3208. ADC0 in VCR schematic. Used for valuable telem data.
-extern MCP_ADC<channels_within_mcp_adc> adc_1; // MCP3208. ADC1 in VCR schematic. Used for extra thermistors or extra sensors while testing.
+using ADC0Instance = etl::singleton<MCP_ADC<channels_within_mcp_adc>>; // MCP3208. ADC0 in VCR schematic. Used for valuable telem data.
+using ADC1Instance = etl::singleton<MCP_ADC<channels_within_mcp_adc>>; // MCP3208. ADC1 in VCR schematic. Used for extra thermistors or extra sensors while testing.
 
 
 #endif /* VCR_GLOBALS */
--- src/VCR_Globals.cpp
+++ src/VCR_Globals.cpp
@@ -9,7 +9,3 @@
 
 /* Interface and system data structs */
 VCRData_s vcr_data;
-
-/* ADC setup */
-MCP_ADC<channels_within_mcp_adc> adc_0 = MCP_ADC<8>(ADC0_CS); // MCP3208. ADC0 in VCR schematic. Used for valuable telem data.
-MCP_ADC<channels_within_mcp_adc> adc_1 = MCP_ADC<8>(ADC1_CS); // MCP3208. ADC1 in VCR schematic. Used for extra thermistors or extra sensors while testing.
