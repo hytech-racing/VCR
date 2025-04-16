e8d55d576f6eba4805d3e63959ff1ff4da48b733
Vansh Aggarwal <vanshagg233@gmail.com>
2025-04-15 21:54:27 -0400

    testing of load cells

--- src/VCR_InterfaceTasks.cpp
+++ src/VCR_InterfaceTasks.cpp
@@ -19,6 +19,8 @@
 #include "AMSSystem.h"
 #include "DrivebrainInterface.h"
 
+bool initailized_adcs = true;
+
 bool init_read_adc0_task()
 {
     float scales[channels_within_mcp_adc] = {GLV_SENSE_SCALE, CURRENT_SENSE_SCALE, REFERENCE_SENSE_SCALE, RL_LOADCELL_SCALE, RR_LOADCELL_SCALE, RL_SUS_POT_SCALE, RR_SUS_POT_SCALE, 1}; //NOLINT
@@ -26,6 +28,7 @@ bool init_read_adc0_task()
     ADC0Instance::create(ADC0_CS, MCP_ADC_DEFAULT_SPI_SDI, MCP_ADC_DEFAULT_SPI_SDO, MCP_ADC_DEFAULT_SPI_CLK, MCP_ADC_DEFAULT_SPI_SPEED, scales, offsets);
     
     hal_printf("Initialized ADC0 at %d (millis)\n", sys_time::hal_millis()); // NOLINT
+    initailized_adcs = false;
     return true;
 }
 
@@ -41,7 +44,9 @@ void run_read_adc0_task()
     vcr_data.interface_data.rear_suspot_data.RL_sus_pot_analog = ADC0Instance::instance().data.conversions[RL_SUS_POT_CHANNEL].raw; // Just use raw for suspots
     vcr_data.interface_data.rear_suspot_data.RR_sus_pot_analog = ADC0Instance::instance().data.conversions[RR_SUS_POT_CHANNEL].raw; // Just use raw for suspots
     
-    hal_printf("ADC0 reading 0 %d\n", ADC0Instance::instance().data.conversions[0].raw); // NOLINT
+    //hal_printf("ADC0 reading 0 %d\n", ADC0Instance::instance().data.conversions[0].raw); // NOLINT
+    //hal_printf("Reading ADC 0 LoadCell RL: %d\n", ADC0Instance::instance().data.conversions[RL_LOADCELL_CHANNEL].raw);
+    Serial.println(static_cast<int>(initailized_adcs));
 }
 
 bool init_read_adc1_task()
@@ -62,7 +67,7 @@ void run_read_adc1_task()
 {
 
     ADC1Instance::instance().tick();
-    hal_printf("ADC1 reading 0 %d\n", ADC1Instance::instance().data.conversions[0].raw);
+    //hal_printf("ADC1 reading 0 %d\n", ADC1Instance::instance().data.conversions[0].raw);
 }
 
 void run_update_buzzer_controller_task()
