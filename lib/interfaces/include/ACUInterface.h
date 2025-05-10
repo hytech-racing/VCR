ad044e9f2191690dec15bbdb6320bf23e24a4271
songyueli <songyuedavidli@gmail.com>
2025-05-10 04:13:25 -0400

    added acu interface for acu heartbeak implementation

---
+++ lib/interfaces/include/ACUInterface.h
@@ -0,0 +1,40 @@
+#ifndef ACUINTERFACE_H
+#define ACUINTERFACE_H
+
+#include "FlexCAN_T4.h"
+
+#include "shared_types.h"
+#include "SharedFirmwareTypes.h"
+#include "etl/singleton.h"
+
+struct ACUCANInterfaceData_s {
+    bool bms_ok;
+    bool imd_ok;
+    uint64_t last_recv_millis; 
+};
+
+class ACUInterface {
+public:
+    ACUInterface() = delete;
+    ACUInterface(uint32_t init_millis, uint32_t max_heartbeat_interval_ms) : _max_heartbeat_interval_ms(max_heartbeat_interval_ms) { 
+        _curr_data.bms_ok = _curr_data.imd_ok = false;
+        _curr_data.last_recv_millis = 0;
+    };
+    void receive_acu_ok_message(const CAN_message_t &msg, unsigned long curr_millis);
+
+    /* Getters */
+    bool is_imd_ok() { return _curr_data.imd_ok; }
+    bool is_bms_ok() { return _curr_data.bms_ok; }
+    uint64_t get_last_received_msg_time() { return _curr_data.last_recv_millis; }
+    bool has_received_first_acu_heartbeat() { return _received_first_acu_heartbeat; }
+    ACUCANInterfaceData_s get_latest_data() { return _curr_data; };
+
+private:
+    ACUCANInterfaceData_s _curr_data;
+    bool _received_first_acu_heartbeat = false;
+    const uint32_t _max_heartbeat_interval_ms;
+};
+
+using ACUInterfaceInstance = etl::singleton<ACUInterface>;
+
+#endif
---
+++ lib/interfaces/src/ACUInterface.cpp
@@ -0,0 +1,12 @@
+#include "ACUInterface.h"
+#include "hytech.h"
+#include "VCRCANInterfaceImpl.h"
+
+void ACUInterface::receive_acu_ok_message(const CAN_message_t &msg, unsigned long curr_millis) {
+    ACU_OK_t acu_msg = {};
+    Unpack_ACU_OK_hytech(&acu_msg, &msg.buf[0], msg.len);
+    _curr_data.imd_ok = acu_msg.imd_ok;
+    _curr_data.bms_ok = acu_msg.bms_ok;
+    if (_curr_data.last_recv_millis == 0) { _received_first_acu_heartbeat = true; }
+    _curr_data.last_recv_millis = curr_millis;
+}
