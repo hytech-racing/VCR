0e23c8a5b24ef8cd8732b8155cbbdb8763f76ff6
Ben Hall <rcmast3r1@gmail.com>
2025-05-31 15:36:24 -0400

    removing power limit from mode 4 and removing sending of inverter control messages onto telem CAN. TODO limit torq in safety mode0 within mode4

--- src/controls.cpp
+++ src/controls.cpp
@@ -12,7 +12,7 @@ VCRControls::VCRControls(DrivetrainSystem *dt_system, uint32_t max_allowed_db_la
         [this](const VCRData_s &state, unsigned long curr_millis) -> DrivetrainCommand_s { return _mode3.evaluate(state, curr_millis); },
         [this](const VCRData_s &state, unsigned long curr_millis) -> DrivetrainCommand_s { return _mode4.evaluate(state, curr_millis); }
     },
-    {true, false, false, false, false}),
+    {false, false, false, false, true}),
     _dt_system(dt_system)
 {
 
