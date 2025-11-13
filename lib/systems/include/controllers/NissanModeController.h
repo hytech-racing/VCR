4976cdfb7e5a38713f69be5a8bcb5a7632aa9d87
Nazar <nazcar101@gmail.com>
2025-11-12 20:00:26 -0500

    actual nissan mode from ht07

--- lib/systems/include/controllers/NissanModeController.h
+++ lib/systems/include/controllers/NissanModeController.h
@@ -3,6 +3,7 @@
 
 #include "PhysicalParameters.h"
 #include "SharedFirmwareTypes.h"
+#include <algorithm>
 
 class NissanModeController
 {
@@ -13,12 +14,12 @@ class NissanModeController
     /// Balanced, 2 = RWD
     /// @param regen_torque_scale same as rear_torque_scale but applies to regen torque split. 0 =
     /// All regen torque on the front, 1 = 50/50, 2 = all regen torque on the rear
-    NissanModeController(float rear_torque_scale, float regen_torque_scale, float slip_threshold)
+    NissanModeController(float rear_torque_scale, float regen_torque_scale)
         : _front_torque_scale(2.0 - rear_torque_scale), _rear_torque_scale(rear_torque_scale),
           _front_regen_torque_scale(2.0 - regen_torque_scale),
-          _rear_regen_torque_scale(regen_torque_scale), _slip_threshold(slip_threshold) {}
+          _rear_regen_torque_scale(regen_torque_scale) {}
     /// @brief default contructor with balanced default values: rear_torque_scale = 1.0, regen_torque_scale = 1.0
-    NissanModeController() : NissanModeController(1.8, 0.6, 1.2) {}
+    NissanModeController() : NissanModeController(1.8, 0.6) {}
 
     DrivetrainCommand_s evaluate(const VCRData_s &vcr_data, unsigned long curr_millis);
 
@@ -29,14 +30,21 @@ class NissanModeController
     const float _rear_regen_torque_scale = 1.0;
     const float _max_amk_rpm = 20000.0f; // TODO do we want to make this a vcr const?
     const float _max_amk_regen = 15.0f;  // TODO do we want to make this a vcr const?
-    const float _slip_threshold = 1.2; // slip threshold multiplier for torque reduction
-    const float _damping = 0.8f; // damping factor for torque compensation - higher means less damped
-    const float _torque_jump_limit = .5f; // N/M jump limit factor for torque compensation
+    
+    
+    float _gtr_def_split = 0.85;
+    float _gtr_alt_split = 0.5;
+    
+    float _fr_slip_clamped;
+    float _fr_slip_factor = 2.5; //lower value allows more slip must be a factor of 5
+    
+    float _f_torque;
+    float _r_torque;
+    
+    float _rear_lr_slip_clamped;
+    float _rear_torque_right_split; //percent of rear torque going to the right side
+    float _rear_slip_factor = 0.5;
 
-    float _last_fl_torque = 0.0f;
-    float _last_fr_torque = 0.0f;
-    float _last_rl_torque = 0.0f;
-    float _last_rr_torque = 0.0f;
 };
 
 #endif
--- lib/systems/src/controllers/NissanModeController.cpp
+++ lib/systems/src/controllers/NissanModeController.cpp
@@ -1,5 +1,5 @@
 #include "controllers/NissanModeController.h"
-#include <algorithm>
+
 
 DrivetrainCommand_s NissanModeController::evaluate(const VCRData_s &vcr_data, unsigned long curr_millis)
 {
@@ -8,24 +8,18 @@ DrivetrainCommand_s NissanModeController::evaluate(const VCRData_s &vcr_data, un
         {0.0f, 0.0f, 0.0f, 0.0f}  // torques
     };
 
+    float max_front_power = PhysicalParameters::AMK_MAX_TORQUE  * _front_torque_scale;
+    float max_rear_power = PhysicalParameters::AMK_MAX_TORQUE  * (2 - _front_torque_scale); 
+
     const PedalsSystemData_s &pedals_data = vcr_data.interface_data.recvd_pedals_data.pedals_data; // get pedals data
     const InverterData_s &fl_inverter_data = vcr_data.interface_data.inverter_data.FL;
     const InverterData_s &fr_inverter_data = vcr_data.interface_data.inverter_data.FR;
     const InverterData_s &rl_inverter_data = vcr_data.interface_data.inverter_data.RL;
     const InverterData_s &rr_inverter_data = vcr_data.interface_data.inverter_data.RR;
 
-    // each motor rpm
-    float fl_rpm = static_cast<float>(fl_inverter_data.speed_rpm);
-    float fr_rpm = static_cast<float>(fr_inverter_data.speed_rpm);
-    float rl_rpm = static_cast<float>(rl_inverter_data.speed_rpm);
-    float rr_rpm = static_cast<float>(rr_inverter_data.speed_rpm);
-    // avg motor rpm
-    float avg_rpm = (fl_rpm + fr_rpm + rl_rpm + rr_rpm) / 4.0f; //NOLINT num motors
-
-
-    // Both pedals are not pressed and no implausibility has been detected
-    // accel_request goes between 1.0 and -1.0
-    float accel_request = pedals_data.accel_percent - pedals_data.brake_percent;
+        // Both pedals are not pressed and no implausibility has been detected
+        // accel_request goes between 1.0 and -1.0
+        float accel_request = pedals_data.accel_percent - pedals_data.brake_percent;
     float torque_request = 0;
 
     if (accel_request >= 0.0) {
@@ -37,76 +31,46 @@ DrivetrainCommand_s NissanModeController::evaluate(const VCRData_s &vcr_data, un
         out.desired_speeds.RL = _max_amk_rpm;
         out.desired_speeds.RR = _max_amk_rpm;
 
-        // calc torque to fl motor
-        if (fl_rpm <= avg_rpm) {
-            float fl_delta_rpm = (avg_rpm - fl_rpm) / avg_rpm; 
-            float fl_compensator = _damping * _last_fl_torque * fl_delta_rpm; // torque value
-            float fl_compensator_bounded = std::min(fl_compensator, _torque_jump_limit);
-            out.torque_limits.FL = std::clamp((torque_request * _front_torque_scale) + fl_compensator_bounded, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
 
+        // begin tv logic
+        float fl_speed_ms = fl_inverter_data.speed_rpm * RPM_TO_METERS_PER_SECOND;
+        float fr_speed_ms = fr_inverter_data.speed_rpm * RPM_TO_METERS_PER_SECOND;
+        float rl_speed_ms = rl_inverter_data.speed_rpm * RPM_TO_METERS_PER_SECOND;
+        float rr_speed_ms = rr_inverter_data.speed_rpm * RPM_TO_METERS_PER_SECOND;
+
+
+        float fr_slip_unclamped = ((rl_speed_ms + rr_speed_ms + 250.0) / (fl_speed_ms + fr_speed_ms + 250.0)) - 1 * _fr_slip_factor;
+        _fr_slip_clamped = std::clamp(fr_slip_unclamped, 0.0f, 1.0f);
+
+        //set front torques
+        _f_torque = 2 * ((1 - _gtr_def_split) * (1 - _fr_slip_clamped) + (1 - _gtr_alt_split) * _fr_slip_clamped) * torque_request;
+
+        //add calculated front torques to the out struct
+        out.torque_limits.FL = _f_torque / 2;
+        out.torque_limits.FR = _f_torque / 2;
+
+        _r_torque = 2 * ((_gtr_def_split)  * (1 - _fr_slip_clamped) + (_gtr_alt_split) * _fr_slip_clamped) * (torque_request);
+        if (rl_speed_ms > rr_speed_ms)
+        {
+            float rear_lr_slip_unclamped = (rl_speed_ms + 250 / rr_speed_ms + 250) - 1 * _rear_slip_factor;
+            _rear_lr_slip_clamped = std::clamp(rear_lr_slip_unclamped, 0.0f, 0.5f);
+            _rear_torque_right_split = 0.5 + _rear_lr_slip_clamped;
         }
-        else if (fl_rpm > avg_rpm * _slip_threshold) {
-            float fl_delta_rpm = (fl_rpm - avg_rpm) / avg_rpm;
-            float fl_compensator = _damping * _last_fl_torque - fl_delta_rpm;
-            out.torque_limits.FL = std::clamp((torque_request * _front_torque_scale), 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else {
-            out.torque_limits.FL = torque_request * _front_torque_scale;
-        }
-        // calc torque to fr motor
-        if (fr_rpm <= avg_rpm) {
-            float fr_delta_rpm = (avg_rpm - fr_rpm) / avg_rpm; 
-            float fr_compensator = _damping * _last_fr_torque * fr_delta_rpm;
-            float fr_compensator_bounded = std::min(fr_compensator, _torque_jump_limit);
-            out.torque_limits.FR = std::clamp((torque_request * _front_torque_scale) + fr_compensator_bounded, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else if (fr_rpm > avg_rpm * _slip_threshold) {
-            float fr_delta_rpm = (fr_rpm - avg_rpm) / avg_rpm;
-            float fr_compensator = _damping * _last_fr_torque * fr_delta_rpm;
-            out.torque_limits.FR = std::clamp((torque_request * _front_torque_scale) + fr_compensator, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else {
-            out.torque_limits.FR = torque_request * _front_torque_scale;
-        }
-        // calc torque to rl motor
-        if (rl_rpm <= avg_rpm) {
-            float rl_delta_rpm = (avg_rpm - rl_rpm) / avg_rpm; 
-            float rl_compensator = _damping * _last_rl_torque * rl_delta_rpm;
-            float rl_compensator_bounded = std::min(rl_compensator, _torque_jump_limit);
-            out.torque_limits.RL = std::clamp((torque_request * _rear_torque_scale) + rl_compensator_bounded, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else if (rl_rpm > avg_rpm * _slip_threshold) {
-            float rl_delta_rpm = (rl_rpm - avg_rpm) / avg_rpm;
-            float rl_compensator = _damping * _last_rl_torque * rl_delta_rpm;
-            out.torque_limits.RL = std::clamp((torque_request * _rear_torque_scale) + rl_compensator, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else {
-            out.torque_limits.RL = torque_request * _rear_torque_scale;
-        }
-        // calc torque to rr motor
-        if (rr_rpm <= avg_rpm) {
-            float rr_delta_rpm = (avg_rpm - rr_rpm) / avg_rpm; 
-            float rr_compensator = _damping * _last_rr_torque * rr_delta_rpm;
-            float rr_compensator_bounded = std::min(rr_compensator, _torque_jump_limit);
-            out.torque_limits.RR = std::clamp((torque_request * _rear_torque_scale) + rr_compensator_bounded, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else if (rr_rpm > avg_rpm * _slip_threshold) {
-            float rr_delta_rpm = (rr_rpm - avg_rpm) / avg_rpm;
-            float rr_compensator = _damping * _last_rr_torque * rr_delta_r  pm;
-            out.torque_limits.RR = std::clamp((torque_request * _rear_torque_scale) + rr_compensator, 0.0f, PhysicalParameters::AMK_MAX_TORQUE);
-        }
-        else {
-            out.torque_limits.RR = torque_request * _rear_torque_scale;
+        else
+        {
+            float rear_lr_slip_unclamped = (rr_speed_ms + 250 / rl_speed_ms + 250) - 1 * _rear_slip_factor;
+            _rear_lr_slip_clamped = std::clamp(rear_lr_slip_unclamped, 0.0f, 0.5f);
+            _rear_torque_right_split = 0.5 - _rear_lr_slip_clamped;
         }
 
-        // store last torques
-        _last_fl_torque = out.torque_limits.FL;
-        _last_fr_torque = out.torque_limits.FR;
-        _last_rl_torque = out.torque_limits.RL;
-        _last_rr_torque = out.torque_limits.RR;
+        //add calculated front torques to the out struct
+        out.torque_limits.RL = _r_torque * (1 - _rear_torque_right_split);
+        out.torque_limits.RR = _r_torque * (_rear_torque_right_split);
 
         return out;
-    } else {
+    } 
+    else 
+    {
         // Negative torque request
         // RPM_TO_METERS_PER_SECOND
 
@@ -119,11 +83,6 @@ DrivetrainCommand_s NissanModeController::evaluate(const VCRData_s &vcr_data, un
         out.torque_limits.RL = torque_request * _rear_regen_torque_scale;
         out.torque_limits.RR = torque_request * _rear_regen_torque_scale;
 
-        //reset last torque to zero to be ready for next accel
-        _last_fl_torque = 0;
-        _last_fr_torque = 0;
-        _last_rl_torque = 0;
-        _last_rr_torque = 0;
         return out;
     }
 }
