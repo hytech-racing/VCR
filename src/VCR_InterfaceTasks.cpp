93ef57667e545576dff293e1b3946ec863225340
songyueli <76572259+songyueli@users.noreply.github.com>
2026-04-15 15:57:45 -0400

    Fix/power limit (#83)
    * adding regen power limit ramp
    
    * small fix
    
    * init load cell construct
    
    * bring back limits
    
    * camel case
    
    * fix
    
    * electrical power limit

--- src/VCR_InterfaceTasks.cpp
+++ src/VCR_InterfaceTasks.cpp
@@ -39,7 +39,6 @@ HT_TASK::TaskResponse run_read_adc0_task(const unsigned long& sysMicros, const H
                                                                 && (ADCInterfaceInstance::instance().get_RR_load_cell().status != AnalogSensorStatus_e::ANALOG_SENSOR_CLAMPED));
 
     vcr_data.interface_data.rear_suspot_data.RL_sus_pot_analog = ADCInterfaceInstance::instance().get_filtered_RL_sus_pot();
-    
     vcr_data.interface_data.rear_suspot_data.RR_sus_pot_analog = ADCInterfaceInstance::instance().get_filtered_RR_sus_pot();
 
     vcr_data.interface_data.current_sensor_data.bspd_brake_high_sense = ADCInterfaceInstance::instance().is_brake_sense_high();
@@ -97,6 +96,10 @@ HT_TASK::TaskResponse update_acu_heartbeat(const unsigned long& sysMicros, const
 {
     ACUCANInterfaceData_s data = ACUInterfaceInstance::instance().get_latest_data(sys_time::hal_millis());
     digitalWrite(SOFTWARE_OK_PIN, data.heartbeat_ok);
+
+    vcr_data.interface_data.stamped_acu_core_data.acu_data.tractive_system_current = data.em_current;
+    vcr_data.interface_data.stamped_acu_core_data.acu_data.max_measured_ts_out_voltage = data.em_voltage;
+
     return HT_TASK::TaskResponse::YIELD;
 }
 
@@ -258,13 +261,8 @@ HT_TASK::TaskResponse enable_motor_cooling(const unsigned long& sysMicros, const
 HT_TASK::TaskResponse enable_inverter_cooling(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo) 
 {
     VehicleState_e vehicle_state = VehicleStateMachineInstance::instance().get_state(); //NOLINT will alway be populated so is ok
-    if (vehicle_state == VehicleState_e::TRACTIVE_SYSTEM_ACTIVE || vehicle_state == VehicleState_e::WANTING_READY_TO_DRIVE || vehicle_state == VehicleState_e::READY_TO_DRIVE) 
-    {
-        digitalWrite(INVERTER_COOLING_CONTROL_PIN, HIGH);
-    }
-    else
-    {
-        digitalWrite(INVERTER_COOLING_CONTROL_PIN, LOW);
-    }
+    bool enable_state = vehicle_state == VehicleState_e::TRACTIVE_SYSTEM_ACTIVE || vehicle_state == VehicleState_e::WANTING_READY_TO_DRIVE || vehicle_state == VehicleState_e::READY_TO_DRIVE;
+    digitalWrite(MOTOR_COOLING_CONTROL_PIN, enable_state ? HIGH : LOW);
+    
     return HT_TASK::TaskResponse::YIELD;
 }
