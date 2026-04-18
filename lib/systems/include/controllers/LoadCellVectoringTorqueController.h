6d0c371426d30ece3d531d3a94d9d444d6767cd0
Nazar <nazcar101@gmail.com>
2026-04-18 14:16:49 -0400

    removed controller specific variables

--- lib/systems/include/controllers/LoadCellVectoringTorqueController.h
+++ lib/systems/include/controllers/LoadCellVectoringTorqueController.h
@@ -41,10 +41,7 @@ private:
     const float _rr_load_cell_scale = 0.137349f;
 
     const float _brake_percent_scale = 1.0f;
-
-    const float _rear_regen_limit = 3.5f;
-    const float _front_regen_limit = 13.0f;
-
+    
     const size_t _max_error_count = 25;
     veh_vec<size_t> _load_cell_error_counts = {};
 };
