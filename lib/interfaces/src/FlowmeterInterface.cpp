a70d788291548eb0f5371f854e0d8ce2fbe1e6b9
songyueli <songyuedavidli@gmail.com>
2026-05-20 23:17:07 -0700

    linter

--- lib/interfaces/src/FlowmeterInterface.cpp
+++ lib/interfaces/src/FlowmeterInterface.cpp
@@ -5,12 +5,11 @@ using namespace default_flowmeter_params;
 FlowmeterInterface::FlowmeterInterface(
     const size_t flowmeter_pin
 ) : 
-    _pin(flowmeter_pin) 
+    _pin(flowmeter_pin),
+    _last_sample_timestamp_ms(0UL)
 {
     pinMode(_pin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(_pin), count_pulse, RISING);
-
-    _last_sample_timestamp_ms = 0;
+    attachInterrupt(digitalPinToInterrupt(_pin), count_pulse, RISING);    
 }
 
 float FlowmeterInterface::get_flow_gpm(unsigned long curr_millis) 
@@ -23,7 +22,7 @@ float FlowmeterInterface::get_flow_gpm(unsigned long curr_millis)
     float comp_samp_frequency = MS_TO_S / static_cast<float>(curr_millis - _last_sample_timestamp_ms);
     _last_sample_timestamp_ms = curr_millis;
 
-    float reading = PULSE_FREQ_TO_GPM * _pulse_count * comp_samp_frequency;
+    float reading = PULSE_FREQ_TO_GPM * static_cast<float>(_pulse_count) * comp_samp_frequency;
     _pulse_count = 0;
 
     return reading;
